e994c2b5966cd356691968d52fabcdd9c072acaa
Dong-Hyun Kim <dhkim95@hotmail.com>
2017-07-14 16:37:37 +0900

    aaa

--- src/main/java/com/dhkim9549/mlpcss/MLPCSS.java
+++ src/main/java/com/dhkim9549/mlpcss/MLPCSS.java
@@ -1,5 +1,24 @@
 package com.dhkim9549.mlpcss;
 
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by internet user on 2017-07-14.
  */
@@ -30,5 +49,110 @@ public class MLPCSS {
         System.out.println("Number of sample size per iteration (nSamples) = " + nSamples);
         System.out.println("i >= 0");
         System.out.println("************************************************");
+
+        MultiLayerNetwork model = getInitModel(learnigRate);
+        //MultiLayerNetwork model = readModelFromFile("/down/ttt_model_h2_uSGD_ge_mb16_ss16_ev100000_aRP_3200000.zip");
+        //MultiLayerNetwork model = readModelFromFile("/down/ttt_model_h2_uSGD_mb16_ss16_5210000.zip");
+
+        NeuralNetConfiguration config = model.conf();
+        System.out.println("config = " + config);
+
+        // training iteration
+        long i = 0;
+
+        long lastIterationModelSave = 0;
+
+        List<DataSet> listDs = getTrainingData();
+
+    }
+
+    public static MultiLayerNetwork getInitModel(double learningRate) throws Exception {
+
+        int seed = 123;
+
+        int numInputs = 9 * 3;
+        int numOutputs = 2;
+        int numHiddenNodes = 9 * 3;
+
+        System.out.println("Build model....");
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .iterations(1)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .learningRate(learningRate)
+                .updater(Updater.SGD)
+                .list()
+                .layer(0, new DenseLayer.Builder().nIn(numInputs).nOut(numHiddenNodes)
+                        .weightInit(WeightInit.XAVIER)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(1, new DenseLayer.Builder().nIn(numHiddenNodes).nOut(numHiddenNodes)
+                        .weightInit(WeightInit.XAVIER)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(2, new DenseLayer.Builder().nIn(numHiddenNodes).nOut(numHiddenNodes)
+                        .weightInit(WeightInit.XAVIER)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(3, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .weightInit(WeightInit.XAVIER)
+                        .activation(Activation.SOFTMAX)
+                        .nIn(numHiddenNodes).nOut(numOutputs).build())
+                .pretrain(false).backprop(true).build();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+
+        return model;
+    }
+
+    private static List<DataSet> getTrainingData() {
+
+        System.out.println("Getting training data...");
+
+        List<DataSet> listDs = new ArrayList<>();
+
+        for (int i = 0; i < nSamples; i++) {
+
+            DataSet ds = getDataSet();
+
+            listDs.add(ds);
+        }
+
+        Collections.shuffle(listDs);
+
+        System.out.println("listDs.size() = " + listDs.size());
+        System.out.println("Getting training complete.");
+
+        return listDs;
+    }
+
+    private static DataSet getDataSet() {
+
+        double[] featureData = new double[2];
+        double[] labelData = new double[2];
+
+        Random rnd = new Random();
+        featureData[0] = (double)rnd.nextInt(2);
+        featureData[1] = (double)rnd.nextInt(2);
+        if(featureData[0] == 0.0 && featureData[1] == 0.0) {
+            labelData[0] = 0.0;
+            labelData[1] = 1.0;
+        } else if(featureData[0] == 1.0 && featureData[1] == 1.0) {
+            labelData[0] = 0.0;
+            labelData[1] = 1.0;
+        } else {
+            labelData[0] = 1.0;
+            labelData[1] = 0.0;
+        }
+
+        INDArray feature = Nd4j.create(featureData, new int[]{1, 2});
+        INDArray label = Nd4j.create(labelData, new int[]{1, 2});
+
+        DataSet ds = new DataSet(feature, label);
+
+        System.out.println("ds = " + ds);
+
+        return ds;
     }
 }
