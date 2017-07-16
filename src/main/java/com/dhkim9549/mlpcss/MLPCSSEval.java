b87a18363fdebabb8c24e3e6c468a000efd36f95
dhkim9549 <dhkim95@hotmail.com>
2017-07-16 11:38:57 +0900

    aaa

--- src/main/java/com/dhkim9549/mlpcss/MLPCSSEval.java
+++ src/main/java/com/dhkim9549/mlpcss/MLPCSSEval.java
@@ -32,6 +32,17 @@ public class MLPCSSEval {
         // Evaluation result output writer
         BufferedWriter out = new BufferedWriter(new FileWriter("/down/list_eval.txt"));
 
+        String header = "";
+        header += "guarnt_no\t";
+        header += "bad_yn\t";
+        header += "income\t";
+        header += "debt\t";
+        header += "cb_grd\t";
+        header += "scor_grd\t";
+        header += "acc_rat\t";
+        out.write(header + "\n");
+        out.flush();
+
         int i = 0;
 
         String s = "";
@@ -42,51 +53,41 @@ public class MLPCSSEval {
                 System.out.println("i = " + i);
             }
 
-            String s2 = "";
+            if(s.startsWith("GUARNT")) {
+                continue;
+            }
 
             String acpt_updt_dy = MLPCSS.getToken(s, 11, "\t");
             if(acpt_updt_dy.compareTo("20150101") < 0) {
                 continue;
             }
 
-            if(s.startsWith("GUARNT")) {
+            String guarnt_no = MLPCSS.getToken(s, 0, "\t");
+            String bad_yn = MLPCSS.getToken(s, 19, "\t");
+            long income = Long.parseLong(MLPCSS.getToken(s, 15, "\t"));
+            long debt = Long.parseLong(MLPCSS.getToken(s, 16, "\t"));
+            long cb_grd = Long.parseLong(MLPCSS.getToken(s, 14, "\t"));
+            long scor_grd = Long.parseLong(MLPCSS.getToken(s, 13, "\t"));
 
-                s2 += "guarnt_no\t";
-                s2 += "bad_yn\t";
-                s2 += "income\t";
-                s2 += "debt\t";
-                s2 += "cb_grd\t";
-                s2 += "scor_grd\t";
-                s2 += "acc_rat\t";
+            double[] featureData = new double[3];
+            featureData[0] = MLPCSS.rescaleAmt(income);
+            featureData[1] = MLPCSS.rescaleAmt(debt);
+            featureData[2] = (double) cb_grd / 10.0;
+            INDArray feature = Nd4j.create(featureData, new int[]{1, 3});
+            INDArray output = model.output(feature);
+            System.out.print("feature = " + feature);
+            System.out.print("  output = " + output);
+            double acc_rat = output.getDouble(0);
+            System.out.println("  acc_rat = " + acc_rat);
 
-            } else {
-
-                String guarnt_no = MLPCSS.getToken(s, 0, "\t");
-                String bad_yn = MLPCSS.getToken(s, 19, "\t");
-                long income = Long.parseLong(MLPCSS.getToken(s, 15, "\t"));
-                long debt = Long.parseLong(MLPCSS.getToken(s, 16, "\t"));
-                long cb_grd = Long.parseLong(MLPCSS.getToken(s, 14, "\t"));
-                long scor_grd = Long.parseLong(MLPCSS.getToken(s, 13, "\t"));
-
-                double[] featureData = new double[3];
-                featureData[0] = MLPCSS.rescaleAmt(income);
-                featureData[1] = MLPCSS.rescaleAmt(debt);
-                featureData[2] = (double) cb_grd / 10.0;
-                INDArray feature = Nd4j.create(featureData, new int[]{1, 3});
-                INDArray output = model.output(feature);
-                System.out.print("feature = " + feature);
-                System.out.print("  output = " + output);
-                double acc_rat = output.getDouble(0);
-                System.out.println("  acc_rat = " + acc_rat);
-
-                s2 += guarnt_no + "\t";
-                s2 += bad_yn + "\t";
-                s2 += income + "\t";
-                s2 += debt + "\t";
-                s2 += cb_grd + "\t";
-                s2 += scor_grd + "\t";
-                s2 += acc_rat + "\t";
-            }
+            String s2 = "";
+            s2 += guarnt_no + "\t";
+            s2 += bad_yn + "\t";
+            s2 += income + "\t";
+            s2 += debt + "\t";
+            s2 += cb_grd + "\t";
+            s2 += scor_grd + "\t";
+            s2 += acc_rat + "\t";
 
             out.write(s2 + "\n");
             out.flush();
